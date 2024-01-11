b1ca1c2c493b4f05b2f9f4ba10917e0ef318f15e
alamin <alaminakash9754@gmail.com>
2024-01-11 10:59:33 +0600

    task delete operation added

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ async function run() {
     app.get('/userTask/:id', async(req, res) => {
       const id = req.params.id;
       const query = {_id: new ObjectId(id)};
-      console.log(query)
+      console.log(query);
       const result = await taskCollection.findOne(query);
       res.send(result);
     })
